8c3ccf803a035d7fd408702dd3c9823ae6114820
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 21:39:06 -0400

    initialization of the app

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDy54GcjZjN-oF8-1jh6P_Y6Ykxawb9ZKQ",
+  authDomain: "amazoreact-40611.firebaseapp.com",
+  databaseURL: "https://amazoreact-40611.firebaseio.com",
+  projectId: "amazoreact-40611",
+  storageBucket: "amazoreact-40611.appspot.com",
+  messagingSenderId: "678896236470",
+  appId: "1:678896236470:web:0044db5d383ea8c419b317",
+  measurementId: "G-6EDJRQ8MWM",
+};

578f8052fe74ad2886f6104c57eaec1cd4e83109
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 23:02:51 -0400

    added the react router

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -2,13 +2,39 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Payment />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ amazon-clone/src/Checkout.js
@@ -0,0 +1,10 @@
+import "./Checkout.css";
+import { useStateValue } from "./StateProvider";
+
+import React from "react";
+
+function Checkout() {
+  return <div className="checkout">checkout page</div>;
+}
+
+export default Checkout;

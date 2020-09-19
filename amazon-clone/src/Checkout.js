93cb19f10d9d3a87eda91f0aa9706214dc10f5cd
tiff0000 <tiffany.yang@mail.utoronto.ca>
2020-09-18 23:20:36 -0400

    added subtotal and reducer

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,10 +1,29 @@
 import "./Checkout.css";
 import { useStateValue } from "./StateProvider";
-
 import React from "react";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
-  return <div className="checkout">checkout page</div>;
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+      </div>
+
+      <div>
+        <h3>Hello, user</h3>
+        <h2 className="checkout__title">Your shopping Basket</h2>
+      </div>
+
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
 }
 
 export default Checkout;
